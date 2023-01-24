339e87125bdc0f3449bcdff017daecfc932450cb
Jaime Mitchell <JaimeMitchell100@gmail.com>
2023-01-24 06:31:00 -0500

    Replacing the filteredData variable with state, to display all fountains in NYC. Notes left on how to use filteredData.

--- src/App.js
+++ src/App.js
@@ -6,14 +6,17 @@ import LocationMarker from './components/LocationMarker';
 
 function App(props) {
   const [geojsonData, setGeojsonData] = useState([]);
+
   console.log(geojsonData);
+
   useEffect(() => {
     fetch('https://data.cityofnewyork.us/resource/bevm-apmm.json')
       .then((response) => response.json())
       .then((data) => setGeojsonData(data));
   }, []);
 
-  const filteredData = geojsonData.filter((feature) => feature.borough === 'B');
+  // const filteredData = geojsonData.filter((feature) => feature.borough === 'M');
+  //Note: filteredData must be used in place of geojsonData for GeoJSON component data value and as the list to map over (currently lines 34 and 40)
 
   return (
     <div id='map'>
@@ -27,15 +30,14 @@ function App(props) {
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
         />
-        <LocationMarker/>
-        <GeoJSON data={filteredData} />
-
+        <LocationMarker />
+        <GeoJSON data={geojsonData} />
         <Marker position={[40.72316, -73.984829394]}>
           <Popup>
             East Village, NYC <br /> Douse around for water
           </Popup>
         </Marker>
-        {filteredData.map((fountain, id) => (
+        {geojsonData.map((fountain, id) => (
           //fountain is 1 object. the_geom is an object . coordinates is a list with 2 values.
           <Marker
             position={[
