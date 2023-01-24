8282f19215cbf8642c3479ac31a677584a1e4d7b
Jaime Mitchell <JaimeMitchell100@gmail.com>
2023-01-23 21:46:23 -0500

    Able to fetch from NYC DATA API, filter it and map over to displ ay markers with relevent info for user.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import { React, useState, useEffect, useContext } from 'react';
 import { MapContainer, TileLayer, GeoJSON, Marker, Popup } from 'react-leaflet';
-import ParkFountains from './components/ParkFountains';
+// import ParkFountains from './components/ParkFountains';
 import './App.css';
 
 function App(props) {
- 
   const [geojsonData, setGeojsonData] = useState([]);
+  console.log(geojsonData);
   useEffect(() => {
-    fetch('https://data.cityofnewyork.us/resource/8kvz-b7tv.json')
+    fetch('https://data.cityofnewyork.us/resource/bevm-apmm.json')
       .then((response) => response.json())
       .then((data) => setGeojsonData(data));
   }, []);
+
+  const filteredData = geojsonData.filter((feature) => feature.borough === 'B');
 
   return (
     <div id='map'>
@@ -24,17 +26,42 @@ function App(props) {
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
         />
-        <GeoJSON data={geojsonData} />
+        <GeoJSON data={filteredData} />
+
         <Marker position={[40.72316, -73.984829394]}>
           <Popup>
             East Village, NYC <br /> Douse around for water
           </Popup>
         </Marker>
-        {/* <ParkFountains
-        /> */}
+        {filteredData.map((fountain, id) => (
+          //fountain is 1 object. the_geom is an object . coordinates is a list with 2 values. why is 1 displayed before 0?
+          <Marker
+            position={[
+              fountain.the_geom.coordinates[1],
+              fountain.the_geom.coordinates[0],
+            ]}
+            key={id}
+          >
+            <Popup>
+              {fountain.signname} <br /> {fountain.descriptio}
+            </Popup>
+          </Marker>
+        ))}
       </MapContainer>
     </div>
   );
 }
 
 export default App;
+
+// {geojsonData.features.map(feature => (
+//   <Marker
+//     key={feature.properties.objectid}
+//     position={[feature.geometry.coordinates[0][0][1], feature.geometry.coordinates[0][0][0]]}>
+// <Popup>
+//   <p>{feature.properties.name}</p>
+//   <p>Zipcode: {feature.properties.zipcode}</p>
+//   <p>Community Board: {feature.properties.communityb}</p>
+//   {/* Add other properties you want to display */}
+// </Popup>
+//     </Marker>))}
