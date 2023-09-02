2956f1ce866b8df3f6733c912ae0a582d9e28ece
Jaime Mitchell <JaimeMitchell100@gmail.com>
2023-09-02 15:09:35 -0400

    changing instructions, but may revert

--- src/components/Instructions.js
+++ src/components/Instructions.js
@@ -8,60 +8,29 @@ const Instructions = () => {
         will,” <br />
         ...but
       </p>
-      Harm has been done.
-      <br /> When you drink from any fountain or fill your jug from a hose bib,
-      <br /> please acknowledge "We The People of the United States",
-      <br /> stole it through violence and genocide, started charging a fee for
-      it, and continue to mistreat it.
-      <br /> Find out more about some of the people who managed and cared for
-      the stolen waters we drink:
       <br />
-      <a href='https://delawaretribe.org/'>Munsee Lenape</a>
-      <br />
-      <a href='https://www.mohican.com/'>
-        Mohican 'The People of the Waters that Are Never Still'
-      </a>
-      <br />
-      <a href='https://www.jeremynative.com/onthissite/wiki/the-tribes-of-long-island/'>
-        “Canarsie”
-      </a>
-      <br />
-      <a href='https://www.schaghticoke.info/'>
-        “Schaghticoke 'Gathered Waters'”
-      </a>
-      <br />
-      <a href='https://en.wikipedia.org/wiki/Montauk_Point_land_claim'>
-        “Montauk”
-      </a>
-      <br />
-      <a href='https://munseedelaware.com/'>Munsee Delaware Grammer Site</a>
-      <br />
-      <a href='https://mountgulian.org/history/the-wappinger-people/'>
-        Wappinger
-      </a>
-      <br />
-      
+
       <ul>
-        <p className='wic'>Other things to consider when using this app:</p>
+        <p className='wic'>landowners, tenants and NYPD get really bent out of shape when you take a few pennies of water.:</p>
+        <p className='wic'>So to avoid a commotion and possible imprisonment over this water:</p>
         <br />
         <li>
-          * Please, if you are using a private water source, get the number on
+          * If you are using a private water source, get the number on
           the door for the super.
         </li>
         <br />
         <li>
-          * Please explain what you'd like to do, show them you have the right
-          size water key, and offer something in return.
+          * Explain what you'd like to do, show them you have the right
+          size water key, and offer something in return if you want.
         </li>
         <br />
         <li>
-          * Examples: if you don't have a few bucks, maybe offer to help take
-          the trash out or hose the sidewalk, etc.
+          * Examples: if you don't have a few bucks, maybe offer to water the street trees
+          or hose the sidewalk, etc.
         </li>
         <br />
         <li>
-          * If it's a non-profit, I usually ask if someone is around and offer a
-          hand. They are excempt from water bills, so tend to be a bit more
+          * If it's a non-profit, they are excempt from water bills, so tend to be a bit more
           generous.
         </li>
         <br />
@@ -70,9 +39,8 @@ const Instructions = () => {
           please ask what the best time is and when the kids are not there.
         </li>
         <br />
-        <li>* Please use the correct size water key.</li>
-        <br />
-        <li>* They are anywhere from $1-8 Dollars. </li>
+        <li>* Please use the correct size water key.They are anywhere from $1-8 Dollars.</li>
+
         <br />
         <li>* Please do not use pliers and strip the valve stems. </li>
         <br />
@@ -89,18 +57,6 @@ const Instructions = () => {
         <a href='https://www.homedepot.com/p/DANCO-5-16-in-Sillcock-Key-80132/300754237?source=shoppingads&locale=en-US&srsltid=Ad5pg_F64O-0nudNoELPfkPhe8JyAIfoogLi2s25PwPYAFMZ6974fE82sX4'>
           Nope, I'm not making a profit if you click on this!
         </a>
-        <br />
-        <br />
-        <li>
-          It will only take me one complaint of damage to the water source,
-          which I'll follow up on, <br />
-          before this becomes another app you have to make an account to use.
-          <br />
-          Right now this is just a database of potable water sources, not
-          people.
-          <br />
-          Let's keep it that way.
-        </li>
         <br />
         <p className='wic'>Thank you for any dousing you do!</p>
       </ul>
